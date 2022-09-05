f54715f790a952903b9fa4679546ddfbe3bc4072
elyutin15 <elyutin15@gmail.com>
2022-09-05 13:57:47 +0300

    spaces

--- src/main/java/zadanie1/Main.java
+++ src/main/java/zadanie1/Main.java
@@ -10,18 +10,22 @@ public class Main {
         int n = sc.nextInt();
         List<Integer> kektor = new ArrayList<>();
         int sum1 = 0, sum2 = 0, sum3 = 0;
+
         for (int i = 0; i < n; ++i) {
             kektor.add(sc.nextInt());
             sum1 += kektor.get(i);
         }
+
         int n2 = n;
         while (--n2 >= 0) {
             sum2 += kektor.get(n2);
         }
+
         n2 = n;
         do {
             sum3 += kektor.get(n2-1);
         } while(--n2>0);
+
         System.out.println(sum1);
         System.out.println(sum2);
         System.out.println(sum3);
